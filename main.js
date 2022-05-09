7ab444722e1df20e54447339b3cad0cceed4cce9
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-09 17:04:52 +0200

    prima analisi richiesta

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+
 //Il computer deve generare 16 numeri casuali tra 1 e 100.
 //I numeri non possono essere duplicati.
+
 //In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //L’utente non può inserire più volte lo stesso numero.
+
 //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// X COMPUTER
+// 1. creo funzione per generare numeri casuali da 1 a 100
+  // 1.2 creo ciclo for per stabilire quanti numeri dovranno essere generati (16) e non dovranno essere ripetuti più volte
+
+// X UTENTE
+// 2. Chiedere all'utente di inserire un numero alla volta per 84 volte (ciclo do while) questi numeri dovranno essere generati sempre compresi fra 1 e 100
+   // 2.1 numeri non possono essere ripetuti
