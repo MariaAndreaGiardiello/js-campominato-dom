6f1a8c9a82d17d6a0eee5d574151e8ba3192bbdb
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-11 15:54:20 +0200

    aggiunto correzione

--- js/main.js
+++ js/main.js
@@ -40,15 +40,14 @@ level = Number(prompt("Scegli la difficoltà: 0 facile - 1 intermedio - 2 diffic
 // 2. Chiedere all'utente di inserire un numero alla volta per 84 volte
 while ( numChoice === false && userNum.length < 3) {
     if ( level === 0 ) {
-        userNumber = randomNumbers(1,100);
         userNumber = Number(prompt("Inserisci un numero compreso fra 1 e 100"));
     } else if ( level === 1 ) {
-        userNumber = randomNumbers(1,80);
         userNumber = Number(prompt("Inserisci un numero compreso fra 1 e 80"));
     } else if ( level === 2 ) {
-        userNumber = randomNumbers(1,50);
         userNumber = Number(prompt("Inserisci un numero compreso fra 1 e 50"));
-    } else if ( numbers.includes(userNumber) ) {
+    }
+    
+    if ( numbers.includes(userNumber) ) {
         // 3 SE i numeri inseriti sono contenuti in quelli generati il numero scleto interromperà il gioco
         numChoice = true;
     } else {
